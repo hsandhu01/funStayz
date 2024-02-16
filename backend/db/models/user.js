a6fec82cef84512b1ae3e014255fb01bfd9f3471
hsandhu01 <hsandhu01@gmail.com>
2024-02-15 19:00:38 -0800

    Add User model scope for user security

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model, Validator } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
@@ -9,45 +8,45 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
 
-  User.init({
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: [4, 30],
-        isNotEmail(value) {
-          if (Validator.isEmail(value)) {
-            throw new Error('Cannot be an email.');
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
           }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
         }
       }
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        len: [3, 256],
-        isEmail: true
-      }
-    },
-    hashedPassword: {
-      type: DataTypes.STRING.BINARY,
-      allowNull: false,
-      validate: {
-        len: [60, 60]
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+        }
       }
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-    defaultScope: {
-      attributes: {
-        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
-      }
-    }
-  });
-
+  );
   return User;
 };
--- backend/db/seeders/20240214043546-demo-user.js
+++ backend/db/seeders/20240214043546-demo-user.js
@@ -1,45 +1,39 @@
 'use strict';
 
-const bcrypt = require('bcryptjs');
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
 
-// Optionally define schema for production environment
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // Adjust according to your actual environment variable
+  options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Users', [
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password', 10), // Adjust the bcrypt salt rounds as needed
-        createdAt: new Date(),
-        updatedAt: new Date()
+        hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2', 10),
-        createdAt: new Date(),
-        updatedAt: new Date()
+        hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3', 10),
-        createdAt: new Date(),
-        updatedAt: new Date()
+        hashedPassword: bcrypt.hashSync('password3')
       }
-    ], options); // Ensure options is passed if using schema
+    ], { validate: true });
   },
 
-  async down(queryInterface, Sequelize) {
-    const Op = Sequelize.Op;
+  async down (queryInterface, Sequelize) {
     options.tableName = 'Users';
-    await queryInterface.bulkDelete(options, {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    });
+    }, {});
   }
 };
