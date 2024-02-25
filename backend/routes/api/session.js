e150ba52155a080f860a288068a011de98fe60b6
hsandhu01 <hsandhu01@gmail.com>
2024-02-25 12:50:05 -0800

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -46,5 +46,11 @@ router.post(
       });
     }
   );
+
+  // Log out
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  });
   
 module.exports = router;
