edac9e261d8588043f6d3c09fc3138590d6f1d76
hsandhu01 <hsandhu01@gmail.com>
2024-02-27 22:02:46 -0800

    test push

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -3,60 +3,55 @@ const { Model, Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    
     static associate(models) {
-      // define association here
+      // define here but not sure if i need first and last name below
     }
-  };
+  }
 
-  User.init(
-    {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [4, 30],
-          isNotEmail(value) {
-            if (Validator.isEmail(value)) {
-              throw new Error("Cannot be an email.");
-            }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error("Cannot be an email.");
           }
         }
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [3, 256],
-          isEmail: true
-        }
-      },
-      hashedPassword: {
-        type: DataTypes.STRING.BINARY,
-        allowNull: false,
-        validate: {
-          len: [60, 60]
-        }
-      },
-      // first name added her
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      // last name added here for phase 5
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
       }
     },
-    {
-      sequelize,
-      modelName: "User",
-      defaultScope: {
-        attributes: {
-          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
-        }
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 256],
+        isEmail: true
       }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    },
+    firstName: { // do i even need this?
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: { // do i need this? it was on phase 5 for authme if not then i think its messing up my code i created a migration for it
+      type: DataTypes.STRING,
+      allowNull: false,
     }
-  );
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: { exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'] },
+    },
+  });
+
   return User;
 };
