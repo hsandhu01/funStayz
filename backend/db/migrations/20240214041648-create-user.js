0e6ee758277dd326fded0a230437aab3ab423a68
hsandhu01 <hsandhu01@gmail.com>
2024-02-28 18:19:42 -0800

    test changed a migrations and userfile

--- backend/db/migrations/20240214041648-create-user.js
+++ backend/db/migrations/20240214041648-create-user.js
@@ -1,5 +1,10 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -15,27 +20,29 @@ module.exports = {
         unique: true
       },
       email: {
-        type: Sequelize.STRING(256), 
+        type: Sequelize.STRING(256),
         allowNull: false,
         unique: true
       },
       hashedPassword: {
-        type: Sequelize.STRING.BINARY, 
+        type: Sequelize.STRING.BINARY,
         allowNull: false
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') 
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') 
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
+
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
   }
 };
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -3,55 +3,50 @@ const { Model, Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    
     static associate(models) {
-      // define here but not sure if i need first and last name below
+      // define association here
     }
-  }
+  };
 
-  User.init({
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [4, 30],
-        isNotEmail(value) {
-          if (Validator.isEmail(value)) {
-            throw new Error("Cannot be an email.");
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
           }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
         }
       }
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 256],
-        isEmail: true
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+        }
       }
-    },
-    hashedPassword: {
-      type: DataTypes.STRING.BINARY,
-      allowNull: false,
-      validate: {
-        len: [60, 60]
-      }
-    },
-    firstName: { // do i even need this?
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: { // do i need this? it was on phase 5 for authme if not then i think its messing up my code i created a migration for it
-      type: DataTypes.STRING,
-      allowNull: false,
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-    defaultScope: {
-      attributes: { exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt'] },
-    },
-  });
-
+  );
   return User;
 };
