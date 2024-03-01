e37c1d8037c94a09a5c1d3cbc624f278e44a1176
hsandhu01 <hsandhu01@gmail.com>
2024-02-29 18:27:45 -0800

    corrected error with schema options

--- backend/db/migrations/20240229203721-create-reviews.js
+++ backend/db/migrations/20240229203721-create-reviews.js
@@ -1,72 +1,67 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production' && process.env.SCHEMA) {
+    options.schema = process.env.SCHEMA;  
+}
+
 module.exports = {
-  async up(queryInterface, Sequelize) {
+    async up(queryInterface, Sequelize) {
+        
+        await queryInterface.createTable('Reviews', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: {
+                        tableName: 'Users',
+                        schema: options.schema
+                    },
+                    key: 'id'
+                },
+                onDelete: 'CASCADE',
+                allowNull: false,
+            },
+            spotId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: {
+                        tableName: 'Spots',
+                        schema: options.schema
+                    },
+                    key: 'id'
+                },
+                onDelete: 'CASCADE',
+                allowNull: false,
+            },
+            review: {
+                type: Sequelize.TEXT,
+                allowNull: false
+            },
+            stars: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+            }
+        }, options);  
+    },
 
-    let options = {};
-
-    
-    if (process.env.NODE_ENV === 'production' && process.env.SCHEMA) {
-      options.schema = process.env.SCHEMA;
+    async down(queryInterface, Sequelize) {
+        
+        await queryInterface.dropTable({ tableName: 'Reviews', ...options });
     }
-
-    await queryInterface.createTable('Reviews', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: { tableName: 'Users', schema: options.schema },
-          key: 'id'
-        },
-        onDelete: 'CASCADE'
-      },
-      spotId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: { tableName: 'Spots', schema: options.schema },
-          key: 'id'
-        },
-        onDelete: 'CASCADE'
-      },
-      review: {
-        type: Sequelize.TEXT,
-        allowNull: false
-      },
-      stars: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    }, options); 
-  },
-
-  async down(queryInterface, Sequelize) {
-    
-    let options = {};
-    if (process.env.NODE_ENV === 'production' && process.env.SCHEMA) {
-      options.schema = process.env.SCHEMA;
-    }
-
-   
-    await queryInterface.dropTable({
-      tableName: 'Reviews',
-      ...options
-    });
-  }
 };
