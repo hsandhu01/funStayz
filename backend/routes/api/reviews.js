ae6579d487bf0ccfa6642e10f3fc3eff496169ed
hsandhu01 <hsandhu01@gmail.com>
2024-04-06 09:43:11 -0700

    cleaned up and fixed review route

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -5,7 +5,6 @@ const { Review, Spot, User, ReviewImage } = require('../../db/models');
 const { requireAuth, restoreUser } = require('../../utils/auth');
 const { handleValidationErrors } = require('../../utils/validation');
 
-
 const validateReview = [
     check('review')
       .exists({ checkFalsy: true })
@@ -15,10 +14,9 @@ const validateReview = [
       .withMessage('Stars are required')
       .isInt({ min: 1, max: 5 })
       .withMessage('Stars must be an integer from 1 to 5'),
-      handleValidationErrors
+    handleValidationErrors
 ];
 
-// this is to check ownership
 const checkReviewOwnership = async (req, res, next) => {
     const review = await Review.findByPk(req.params.reviewId);
     if (!review) {
@@ -31,7 +29,6 @@ const checkReviewOwnership = async (req, res, next) => {
     next();
 };
 
-// get all reviews of user current
 router.get('/current', restoreUser, requireAuth, async (req, res) => {
     const userId = req.user.id;
     const reviews = await Review.findAll({
@@ -45,47 +42,41 @@ router.get('/current', restoreUser, requireAuth, async (req, res) => {
     res.json({ Reviews: reviews });
 });
 
-// get all reviews on spot id
 router.get('/spots/:spotId/reviews', async (req, res) => {
     const { spotId } = req.params;
     const reviews = await Review.findAll({
         where: { spotId },
         include: [
             { model: User, as: 'User', attributes: ['id', 'firstName', 'lastName'] },
-            { model: Image, as: 'ReviewImages', attributes: ['id', 'url'] }
+            { model: ReviewImage, as: 'ReviewImages', attributes: ['id', 'url'] }
         ]
     });
     res.json({ Reviews: reviews });
 });
 
-//// create review based on id
-// router.post('/spots/:spotId/reviews', restoreUser, requireAuth, validateReview, async (req, res) => {
-//     const { spotId } = req.params;
-//     const { review, stars } = req.body;
-//     const userId = req.user.id;
+router.post('/spots/:spotId/reviews', restoreUser, requireAuth, validateReview, async (req, res) => {
+    const { spotId } = req.params;
+    const { review, stars } = req.body;
+    const userId = req.user.id;
 
-//     // check on existence of spot
-//     const spot = await Spot.findByPk(spotId);
-//     if (!spot) {
-//         return res.status(404).json({ message: "Spot couldn't be found" });
-//     }
+    const spot = await Spot.findByPk(spotId);
+    if (!spot) {
+        return res.status(404).json({ message: "Spot couldn't be found" });
+    }
 
-//     // check if reviewed already
-//     const existingReview = await Review.findOne({ where: { userId, spotId } });
-//     if (existingReview) {
-//         return res.status(500).json({ message: "User already has a review for this spot" });
-//     }
+    const existingReview = await Review.findOne({ where: { userId, spotId } });
+    if (existingReview) {
+        return res.status(403).json({ message: "User already has a review for this spot" });
+    }
 
-//     const newReview = await Review.create({ userId, spotId, review, stars });
-//     res.status(201).json(newReview);
-// });
+    const newReview = await Review.create({ userId, spotId, review, stars });
+    res.status(201).json(newReview);
+});
 
-// add image review on review id
 router.post('/:reviewId/images', restoreUser, requireAuth, checkReviewOwnership, async (req, res) => {
     const { url } = req.body;
     const { reviewId } = req.params;
 
-    // max images limit to 10
     const images = await ReviewImage.findAll({ where: { reviewId } });
     if (images.length >= 10) {
         return res.status(403).json({ message: "Maximum number of images for this resource was reached" });
@@ -95,14 +86,12 @@ router.post('/:reviewId/images', restoreUser, requireAuth, checkReviewOwnership,
     res.status(200).json(image);
 });
 
-// edit review
-router.put('/:reviewId', restoreUser, requireAuth, checkReviewOwnership, validateReview ,async (req, res) => {
+router.put('/:reviewId', restoreUser, requireAuth, checkReviewOwnership, validateReview, async (req, res) => {
     const { review, stars } = req.body;
     await req.review.update({ review, stars });
     res.json(req.review);
 });
 
-// del review
 router.delete('/:reviewId', restoreUser, requireAuth, checkReviewOwnership, async (req, res) => {
     await req.review.destroy();
     res.json({ message: "Successfully deleted" });
